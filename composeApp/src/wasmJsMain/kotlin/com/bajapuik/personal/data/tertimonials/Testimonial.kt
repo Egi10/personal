f63c828be2c21b9f5705d1c17874725e150d0dee
Egi10 <egifcb@gmail.com>
2025-04-10 17:27:04 +0700

    feat(data): Add more testimonials data

--- composeApp/src/wasmJsMain/kotlin/com/bajapuik/personal/data/tertimonials/Testimonial.kt
+++ composeApp/src/wasmJsMain/kotlin/com/bajapuik/personal/data/tertimonials/Testimonial.kt
@@ -14,6 +14,18 @@ object TestimonialData {
             name = "Wildan Angga Rahman",
             role = "Mobile Engineer",
             image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV7rJh13ruHHHOmrbO2mKI7j8cy-lcJ3HTrQ&s"
+        ),
+        Testimonial(
+            message = "Julsaparigi have a great insting as logical roles, he can balance beetween feels and logic to connected with my design and deliver the smooth interaction using his code",
+            name = "Permana Handiyuda",
+            role = "Interactin Designer II",
+            image = "https://framerusercontent.com/images/APE9IHS2cgALQ3CPofVr9lmqnc.png?scale-down-to=512"
+        ),
+        Testimonial(
+            message = "Egi sangat berdedikasi dengan dunia mobile engineering, tiap tantangan dia jawab dengan sungguh sungguh, hasil karyanya mulai dari refactor KMP hingga jetpack compose membuat codebase kita modern dan baik sehingga minim error dan mampu mendeliver features dengan baik.",
+            name = "Mohamad Damba Putrabangga",
+            role = "Senior Engineering Manager",
+            image = "https://media.licdn.com/dms/image/v2/C5603AQES8LPrXNquMw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1517436250905?e=1749686400&v=beta&t=2tlXl3HkGldHSU8dp7M1FG6ZTpO4JPYCgCLsPvtxRcE"
         )
     )
 }
